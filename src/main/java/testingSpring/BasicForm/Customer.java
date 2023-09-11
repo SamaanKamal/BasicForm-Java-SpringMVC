aedf604d3d050c78e65b6b1b17c77ae01fc78f48
SamaanKamal <samaankamal72@gmail.com>
2023-09-11 19:45:47 +0300

    added postal code with regax validation and free passes with min and max validations

--- src/main/java/testingSpring/BasicForm/Customer.java
+++ src/main/java/testingSpring/BasicForm/Customer.java
@@ -1,7 +1,6 @@
 package testingSpring.BasicForm;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 
 public class Customer {
@@ -10,6 +9,11 @@ public class Customer {
     @Size(min = 1,message = "is required")
     private String lastName;
     private String country;
+    @Min(value = 1,message = "must be greater than or equal 1")
+    @Max(value = 10,message = "must be less than or equal 10")
+    private int freePasses;
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
     public Customer(){}
 
     public String getFirstName() {
@@ -34,5 +38,21 @@ public class Customer {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
